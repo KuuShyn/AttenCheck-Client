a432de9796ca79994e42d1f7d21ee8d18af3a16a
KuuShyn <magalonakaj@gmail.com>
2024-07-30 04:34:57 +0800

    next auth implementation

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,97 +1,98 @@
-import { Backend_URL } from "@/lib/Constants";
-import { NextAuthOptions } from "next-auth";
-import { JWT } from "next-auth/jwt";
-import NextAuth from "next-auth/next";
-import CredentialsProvider from "next-auth/providers/credentials";
+import { Backend_URL } from '@/lib/Constants';
+import { NextAuthOptions } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 async function refreshToken(token: JWT): Promise<JWT> {
-  try {
-    const res = await fetch(`${Backend_URL}/auth-v2/refresh`, {
-      method: "POST",
-      headers: {
-        authorization: `Refresh ${token.backendTokens.refreshToken}`,
-      },
-    });
+	try {
+		const res = await fetch(`${Backend_URL}/auth-v2/refresh`, {
+			method: 'POST',
+			headers: {
+				authorization: `Refresh ${token.backendTokens.refreshToken}`,
+			},
+		});
 
-    if (!res.ok) {
-      throw new Error(`Failed to refresh token: ${res.statusText}`);
-    }
+		if (!res.ok) {
+			throw new Error(`Failed to refresh token: ${res.statusText}`);
+		}
 
-    const response = await res.json();
+		const response = await res.json();
 
-    return {
-      ...token,
-      backendTokens: response.backendTokens,
-    };
-  } catch (error) {
-    console.error("Error refreshing token", error);
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
+		return {
+			...token,
+			backendTokens: response.backendTokens,
+		};
+	} catch (error) {
+		console.error('Error refreshing token', error);
+		return {
+			...token,
+			error: 'RefreshAccessTokenError',
+		};
+	}
 }
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "jsmith",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.username || !credentials?.password) return null;
+	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				username: {
+					label: 'Username',
+					type: 'text',
+					placeholder: 'jsmith',
+				},
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				if (!credentials?.username || !credentials?.password)
+					return null;
 
-        const { username, password } = credentials;
-        const res = await fetch(`${Backend_URL}/auth-v2/login`, {
-          method: "POST",
-          body: JSON.stringify({
-            username,
-            password,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        if (res.status == 401) return null;
+				const { username, password } = credentials;
+				const res = await fetch(`${Backend_URL}/auth-v2/login`, {
+					method: 'POST',
+					body: JSON.stringify({
+						username,
+						password,
+					}),
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				});
+				if (res.status == 401) return null;
 
-        const user = await res.json();
-        return user;
-      },
-    }),
-  ],
+				const user = await res.json();
+				return user;
+			},
+		}),
+	],
 
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        return { ...token, ...user };
-      }
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				return { ...token, ...user };
+			}
 
-      const currentTime = new Date().getTime();
-      if (currentTime < token.backendTokens.expiresIn) {
-        return token;
-      }
+			const currentTime = new Date().getTime();
+			if (currentTime < token.backendTokens.expiresIn) {
+				return token;
+			}
 
-      return await refreshToken(token);
-    },
+			return await refreshToken(token);
+		},
 
-    async session({ session, token }) {
-      session.user = token.user;
-      session.backendTokens = token.backendTokens;
+		async session({ session, token }) {
+			session.user = token.user;
+			session.backendTokens = token.backendTokens;
 
-      return session;
-    },
-  },
+			return session;
+		},
+	},
 
-  pages: {
-    signIn: "/signin",
-    signOut: "/signout",
-  },
+	pages: {
+		signIn: '/signin',
+		signOut: '/signout',
+	},
 };
 
 const handler = NextAuth(authOptions);
