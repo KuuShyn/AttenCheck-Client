3dcc92fd1ebf4c41afb02f0d4c7a7329aa275eae
KuuShyn <magalonakaj@gmail.com>
2024-08-07 15:13:57 +0800

    dumboo lol

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -95,5 +95,5 @@ export const authOptions: NextAuthOptions = {
 	},
 };
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
